16b5175496747f165f1d6aa8861f1a683c272931
Prathameshghorapade <pghorapade17@gmail.com>
2025-07-15 00:07:31 +0530

    Patters

---
+++ Assignment 3/For Loop/Ex1.java
@@ -0,0 +1,23 @@
+class Ex1{
+
+public static void main(String args[]){
+
+
+int i;
+int j;
+
+for (i = 5; i >= 1; i--) {
+    for (j = 4; j >= 1; j--) {
+        System.out.println();
+    }
+}
+System.out.print(i + " " + j + ""); 
+
+
+
+
+
+
+}
+
+}
---
+++ Patterns/patterns.java
@@ -0,0 +1,440 @@
+class patterns{
+
+public static void main(String []args){
+
+
+
+
+//11111
+//22222
+//33333
+//44444
+//55555
+                                                      
+
+    /* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print(i);
+}
+
+System.out.println();
+
+} */
+
+
+
+
+
+
+// 12345
+// 12345
+// 12345
+// 12345
+// 12345
+
+
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print(j);
+}
+
+System.out.println();
+
+} */
+
+
+
+
+
+
+
+
+// 54321
+// 54321
+// 54321
+// 54321
+// 54321
+
+
+/*for(int i=1;i<=5;i++)
+{
+
+for(int j=5;j>=1;j--)
+
+{
+
+ System.out.print(j);
+}
+
+System.out.println();
+
+} */
+
+
+
+
+// 55555
+// 44444
+// 33333
+// 22222
+// 11111
+
+
+/*for(int i=5;i>=1;i--)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print(i);
+}
+
+System.out.println();
+
+} */
+
+
+// Unicode Value of A-->65
+
+
+// AAAAA
+// BBBBB
+// CCCCC
+// DDDDD
+// EEEEE
+
+
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print((char)(64+i));
+}
+
+System.out.println();
+
+} */
+
+
+
+//ABCDE
+//ABCDE
+//ABCDE
+//ABCDE
+//ABCDE
+
+ /*for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print((char)(64+j));
+}
+
+System.out.println();
+
+} */
+
+
+
+//EDCBA
+//EDCBA
+//EDCBA
+//EDCBA
+//EDCBA
+
+
+
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=5;j>=1;j--)
+
+{
+
+ System.out.print((char)(64+j));
+}
+
+System.out.println();
+
+} */
+
+
+
+// Unicode Value of a-->97
+
+//abcde
+//abcde
+//abcde
+//abcde
+//abcde
+
+
+
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print((char)(96+j));
+}
+
+System.out.println();
+
+} */
+
+
+
+
+//edcba
+//edcba
+//edcba
+//edcba
+//edcba
+
+
+ /* for(int i=1;i<=5;i++)
+{
+
+for(int j=5;j>=1;j--)
+
+{
+
+ System.out.print((char)(96+j));
+}
+
+System.out.println();
+
+} */
+
+
+
+
+//aaaaa
+//bbbbb
+//ccccc
+//ddddd
+//eeeee
+
+
+/*for(int i =1;i<=5;i++){
+
+for(int j=1;j<=5;j++){
+
+ System.out.print((char)(96+i));
+
+
+}
+
+System.out.println();
+
+
+}*/
+
+
+
+
+
+
+
+//eeeee
+//ddddd
+//ccccc
+//bbbbb
+//aaaaa
+
+
+for(int i=5;i>=1;i--){
+
+
+for(int j=1;j<=5;j++){
+
+System.out.print((char)(96+i));
+
+}
+
+System.out.println();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+}
+
+System.out.println();
+
+} */
+
+
+
+
+
+// *****   *****
+// *****   *****
+// *****   *****
+// *****   *****
+// *****   *****
+
+
+
+
+ 
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+}
+
+System.out.print("\t");
+
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+
+}
+
+
+System.out.println();
+
+
+} */
+
+
+
+
+
+
+// *****   *****   *****   *****   *****
+// *****   *****   *****   *****   *****
+// *****   *****   *****   *****   *****
+// *****   *****   *****   *****   *****
+// *****   *****   *****   *****   *****
+
+
+ 
+/* for(int i=1;i<=5;i++)
+{
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+}
+
+System.out.print("\t");
+
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+
+}
+
+System.out.print("\t");
+
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+
+}
+
+System.out.print("\t");
+
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+
+}
+
+
+System.out.print("\t");
+
+
+for(int j=1;j<=5;j++)
+
+{
+
+ System.out.print("*");
+
+}
+
+System.out.println();
+
+} */
+
+
+
+
+
+
+
+
+
+ 
+   }
+
+
+  }
